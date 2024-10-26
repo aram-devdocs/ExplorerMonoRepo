75e263f8aee5caa734020cb1b7cda39dd09f7613
Aram Hammoudeh <aram.hammoudeh@indr.com>
2024-10-26 00:07:46 -0600

    fix db connection

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting development server..."
-export FLASK_APP=apps.flask_api.app 
+# export FLASK_APP=apps.flask_api.app 
 export FLASK_ENV=development
-flask run
+flask --app apps/flask_api run
